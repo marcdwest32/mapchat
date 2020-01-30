422210f8a36ac174e742eea4de18bb965edd4092
Your marcdwest32 <marcdwest@gmail.com>
2020-01-29 20:05:03 -0600

    add ScrollView

--- client/src/screens/ListScreen.js
+++ client/src/screens/ListScreen.js
@@ -1,8 +1,25 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
+
 import MessagePreview from '../components/MessagePreview';
 
 const messages = [
+  {
+    username: 'Zoidberg',
+    title: 'Need a friend?',
+    text:
+      'Why not Zoidberg? I have so many things to say. They will take up more than one line.',
+  },
+  {
+    username: 'Bender',
+    title: 'Can it, meatbag',
+    text: 'Bite my shiny metal ass',
+  },
+  {
+    username: 'Professor Farnsworth',
+    title: 'Bad news, nobody',
+    text: `I still don't understand why you wouldn't let me graft a laser cannon on your chest, to crush those who disobey you! But I guess we're just two different people.`,
+  },
   {
     username: 'Zoidberg',
     title: 'Need a friend?',
@@ -23,14 +40,14 @@ const messages = [
 
 const ListScreen = screenProps => {
   return (
-    <View>
+    <ScrollView>
       <MessagePreview messages={messages} />
-    </View>
+    </ScrollView>
   );
 };
 
 ListScreen.navigationOptions = {
-  title: 'List',
+  title: 'Nearby Messages',
 };
 
 export default ListScreen;
