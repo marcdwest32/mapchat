0105780aebc0cbd3f90d82c16bc3d255839c90d0
Your marcdwest32 <marcdwest@gmail.com>
2020-07-07 18:23:50 -0500

    dummy data

--- client/src/screens/FriendsScreen.js
+++ client/src/screens/FriendsScreen.js
@@ -1,53 +1,58 @@
-import React from "react";
+import React from 'react';
 import {
   StyleSheet,
   ScrollView,
   View,
-  KeyboardAvoidingView
-} from "react-native";
-import { Avatar, Card, Divider, Text } from "react-native-paper";
+  KeyboardAvoidingView,
+} from 'react-native';
+import { Avatar, Card, Divider, Text } from 'react-native-paper';
 
 const FriendsScreen = ({ screenProps }) => {
   const { user } = screenProps;
 
   friends = [
     {
-      bio: "Here for the haikus",
-      email: "autumndupin@gmail.com",
+      bio:
+        'Quasar encyclopaedia galactica cosmic fugue astonishment worldlets Neque porro quisquam est.',
+      email: 'consequatur@quasi.com',
       id: 2,
-      name_first: "Autumn",
-      name_last: "Dupin",
-      status: "Exploring",
-      username: "addupe"
-    },
-    {
-      bio: "Executive Delivery Boy",
-      email: "cusword32@gmail.com",
-      id: 3,
-      name_first: "Philip J.",
-      name_last: "Fry",
-      status: "Icy",
-      username: "Captain Yesterday"
+      name_first: 'Quid',
+      name_last: 'Dolorem',
+      status: 'Quis autem vel eum iure reprehenderit',
+      username: 'consequatur',
     },
     {
       bio:
-        "Chef. Horse Whisperer. Philanthropist. Software Engineer. Boy Wonder",
-      email: "ryanmoragas@gmail.com",
-      id: 4,
-      name_first: "Ryan",
-      name_last: "Moragas",
-      status: "Pushing Pixels",
-      username: "CrynRyan"
+        'Parturient posuere natoque vestibulum mi maecenas sollicitudin etiam sociis senectus.',
+      email: 'hendrerit@sociosqu.com',
+      id: 3,
+      name_first: 'Risus',
+      name_last: 'Cursus',
+      status: 'Aptent rutrum, nisi vivamus lobortis porta!',
+      username: 'RisusCursus',
     },
     {
-      bio: "Coding",
-      email: "jilliantish@gmail.com",
+      bio:
+        'Sit at mauris dolor ipsum faucibus fringilla tempus, tempus senectus.',
+      email: 'Urna@sociis.com',
+      id: 4,
+      name_first: 'Conubia',
+      name_last: 'Egestas',
+      status:
+        'Ullamcorper iaculis, ipsum adipiscing. Malesuada porta mollis mattis sem varius nunc viverra condimentum fringilla natoque.',
+      username: 'ConubiaEgestas',
+    },
+    {
+      bio:
+        'Natoque mattis habitasse, sagittis feugiat a volutpat sollicitudin. Primis aptent inceptos ligula class convallis fermentum orci hac curabitur consectetur.',
+      email: 'jilliantish@gmail.com',
       id: 5,
-      name_first: "Jill",
-      name_last: "Poole",
-      status: "Hacking Away",
-      username: "JillyBean"
-    }
+      name_first: 'Magna',
+      name_last: 'Integer',
+      status:
+        'Cras ac viverra convallis auctor risus volutpat senectus magna integer auctor vel porttitor.',
+      username: 'MagnaInteger',
+    },
   ];
 
   return (
@@ -58,8 +63,8 @@ const FriendsScreen = ({ screenProps }) => {
           return (
             <Card style={styles.container} key={friend.id}>
               <View>
-                <KeyboardAvoidingView behavior="position" enabled>
-                  <Card style={{ backgroundColor: "#F5F0F6" }}>
+                <KeyboardAvoidingView behavior='position' enabled>
+                  <Card style={{ backgroundColor: '#F5F0F6' }}>
                     <Card.Title
                       title={`${friend.name_first} ${friend.name_last}`}
                       subtitle={friend.email}
@@ -76,8 +81,8 @@ const FriendsScreen = ({ screenProps }) => {
                       <Text
                         style={{
                           paddingBottom: 7,
-                          fontWeight: "bold",
-                          fontSize: 16
+                          fontWeight: 'bold',
+                          fontSize: 16,
                         }}
                       >
                         Status: {friend.status}
@@ -87,8 +92,8 @@ const FriendsScreen = ({ screenProps }) => {
                         style={{
                           paddingTop: 7,
                           paddingBottom: 7,
-                          fontWeight: "bold",
-                          fontSize: 16
+                          fontWeight: 'bold',
+                          fontSize: 16,
                         }}
                       >
                         Bio: {friend.bio}
@@ -110,23 +115,23 @@ const styles = StyleSheet.create({
     marginTop: 10,
     padding: 10,
     paddingBottom: 10,
-    backgroundColor: "#385F71"
+    backgroundColor: '#385F71',
   },
-  avatar: { backgroundColor: "#D7B377" },
+  avatar: { backgroundColor: '#D7B377' },
   baseText: {
-    color: "#D7B377",
+    color: '#D7B377',
     fontSize: 20,
-    fontWeight: "bold"
-  }
+    fontWeight: 'bold',
+  },
 });
 
 FriendsScreen.navigationOptions = {
-  title: "Friends",
-  headerTintColor: "#D7B377",
+  title: 'Friends',
+  headerTintColor: '#D7B377',
   headerStyle: {
-    backgroundColor: "#2B4162"
+    backgroundColor: '#2B4162',
   },
-  headerRight: <Text style={styles.baseText}> MapChat </Text>
+  headerRight: <Text style={styles.baseText}> MapChat </Text>,
 };
 
 export default FriendsScreen;
